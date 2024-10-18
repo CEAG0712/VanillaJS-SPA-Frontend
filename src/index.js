7f4114f6fde080a22f61e9c8096ea8d589fde781
Carlos Arosemena <carlosarosemena@Carloss-MacBook-Pro-2.local>
2024-10-17 20:00:36 -0400

    webpack config done

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./styles/stylekanban.css";
+
+const testH1 = document.getElementById("test-h1");
+testH1.innerHTML = "Configure Webpack + Amanda";
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+
+    port: 3002,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Kanban Board",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
