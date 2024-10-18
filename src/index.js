7bec9cb4a182ba924d0821055bcd22b856de9583
Carlos Arosemena <carlosarosemena@Carloss-MacBook-Pro-2.local>
2024-10-18 17:12:17 -0400

    routing-part-3

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ export const router = async () => {
   const path = location.pathname || "/";
   const route = routes[path];
 
+  try {
+    const res = await fetch(route.path);
+    const html = await res.text();
+    app.innerHTML = html;
+  } catch (error) {
+    app.innerHTML = "<h1> 404 PAGE NOT FOUND </h1>";
+  }
 };
 
 router();
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -48,5 +49,8 @@ module.exports = {
       template: "./src/index.html",
     }),
     new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/pages", to: "pages" }],
+    }),
   ],
 };
