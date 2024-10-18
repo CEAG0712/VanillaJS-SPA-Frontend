624e975bafe9ee51095f557b7bf957b2fef951c0
Carlos Arosemena <carlosarosemena@Carloss-MacBook-Pro-2.local>
2024-10-18 17:31:12 -0400

    routing-part-4

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,12 @@ const routes = {
   "/": {
     path: "/pages/landing.html",
   },
+  "/register": {
+    path: "/pages/auth/register.html",
+  },
+  "/login": {
+    path: "/pages/auth/login.html",
+  },
 };
 
 export default routes;
