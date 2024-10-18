4a2be13640fc0d8c394c92368a9d8ea05fdf2d2e
Carlos Arosemena <carlosarosemena@Carloss-MacBook-Pro-2.local>
2024-10-18 13:49:33 -0400

    routing Part II

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import "./styles/stylekanban.css";
+import routes from "./routes.js";
 
+//arrow function - 2015
+export const router = async () => {
+  const app = document.getElementById("app");
+  const path = location.pathname || "/";
+  const route = routes[path];
 
+};
+
+router();
